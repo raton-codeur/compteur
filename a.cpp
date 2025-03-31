55e93c3a0868198ac994104ee8e218bc72bb8466
Quentin Hauuy <quentinhauuy@hotmail.com>
2025-03-31 08:57:33 +0200

    blabla

---
+++ a.cpp
@@ -0,0 +1,20 @@
+
+#include <iostream>
+#include <unistd.h>
+
+int main()
+{
+	int secondes = 0;
+	int minutes = 0;
+    while (true)
+	{
+        std::cout << '\r' << minutes << " : " << secondes << std::flush;
+        sleep(1);
+		secondes++;
+		if (secondes == 5)
+		{
+			secondes = 0;
+			minutes++;
+		}
+    }
+}
